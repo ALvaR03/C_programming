ef15199c3cdb94007e2a7dd30a047f8102328277
Alvaro Lima <alvarolima1103@outlook.com>
2020-02-25 11:34:27 -0400

    adicionando novo arquivo

--- ativ1/exerc20.c
+++ ativ1/exerc20.c
@@ -10,67 +10,25 @@
 int main(void) {
 	system("clear");
 
-	int i;
-	int a[6], b[6], c[6];
+	int a[6], b[6], c[7], i;
 
 	//Inserir os dados
 	for (i = 0; i <= 6; i++) {
 		printf("Coloque o %d numero do primeiro array: ", i+1);
 		scanf("%d", &a[i]);
+		printf("Coloque o %d numero do segundo array: ", i+1);
+		scanf("%d", &b[i]);
+
+		c[i] = a[i] + b[i];
 	}
 
-	printf("\n\n");
-
-		//Inserir os dados
-		for (i = 0; i <= 6; i++) {
-			printf("Coloque o %d numero do segundo array: ", i+1);
-			scanf("%d", &b[i]);
-		}
-
 		printf("\n\n");
-
-			//processar os dados no array
-			for (i = 0; i <= 6; i++) {
-				c[i] = a[i] + b[i];
-			}
 
 			i = 0;
 			do {
 				printf("c[%d] = %d \n", i, c[i]);
 			i++;
-			} while (i <= 6);
-				//exibir na tela o resultado
-			//	for (i = 0; i <= 6; i++) {
-			//	}
+			} while (i < 7);
 	return 0;
 
-
-/*	=======================
-	int a[6], b[6], c[6], j = 0;	
-
-	int i = 0;
-	while(i <= 6) {
-		printf("Digite o %d numero do primeiro array: ", i+1);
-		scanf("%d", &a[i]);
-		i += 1;	
-	}
-
-	printf("\n\n");
-
-		i = 0;
-		while(i <= 6) {
-			printf("Digite o %d numero do segundo array: ", i+1);
-			scanf("%d", &b[i]);
-			i += 1;	
-		}
-
-		printf("\n\n");
-
-			i = 0;
-			printf("O resultados dos arrays em e:\n");
-			for (j = 0; j <= 6; j++) {
-				c[i] = a[i] + b[i];
-				printf("c[%d] = %d \n\n", j, c[i]);
-			}
-*/
 }
--- ordemAlfab.c
+++ ordemAlfab.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> // Adiciona funcoes especiais com o strcpy
+
+	/* Escreva um algoritmo que leia 10 nomes e exiba
+	 * na tela os nomes em ordem alfabetica */
 
 int main(){
-	
+	system("clear");
 
+	char nome[99];
+	int i, j;
+	char n;
+
+	/* Entrada de dados */
+
+	for (i = 0; i <= 4; i++) {
+		printf("Escreva um nome: ");
+		scanf("%s", &nome[i]);
+	}
+		/* Ordenacao */
+
+		for (i = 0; i <= 4; i++) {
+			for (j = i + 1; j <= 4; j++) {
+
+				if (nome[i] > nome[j]) {
+					n = nome[i];
+					nome[i] = nome[j];
+					nome[j] = n;
+				}
+
+			}
+		}
+
+		/* Saida de dados */
+
+		for (i = 0; i <= 4; i++) 
+			printf("%s", nome[i]);
+
+	return 0;
 }
