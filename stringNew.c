b916433b090d1e670aa61f65faf7c590db45dfec
Alvaro Lima <alvarolima1103@outlook.com>
2020-02-26 21:35:14 -0400

    Exemplo da biblioteca string.h

--- stringNew.c
+++ stringNew.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+		/* A biblioteca string.h */
 
 int main(void) {
+	system("clear");
 
+	char nome[15];
+
+	/* strcpy(string_origem, "string_distino")*/
+	strcpy(nome, "Bruce Wane");
+
+	printf("Nome: %s \n", nome);
+
+
+	
+	/*
 	char palavra[7] = {"Brasil"};
 	char palavra[] = {"Brasil"};
 	char palavra[] = "Brasil";
 
 	printf("%s \n", palavra);
+	*/
 
 	return 0;
 }
