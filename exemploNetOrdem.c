0700086603e409fa0d21563a99c4e7c7f3055b9f
Alvaro Lima <alvarolima1103@outlook.com>
2020-03-12 22:54:19 -0400

    adicionado a atividade extra

--- exemploNetOrdem.c
+++ exemploNetOrdem.c
@@ -5,17 +5,17 @@
 int main(void) {
 	system("clear");
 
-	char nm[3][20], aux[20];
+	char nm[6][20], aux[20];
 	int i, j;
 
-	for (i = 0; i < 3; i++) {
-	   printf("\nNome %i: ", i+1);
+	for (i = 0; i < 6; i++) {
+	   printf("Nome %i: ", i+1);
 	   scanf("%s", nm[i]);
 	}
 
 
-	for (i = 1; i < 3; i++) { /* 3 = qtde de palavras */
-	   for (j = 1; j < 3; j++) {
+	for (i = 1; i < 6; i++) { /* 3 = qtde de palavras */
+	   for (j = 1; j < 6; j++) {
 		 // verifica se tem que ser depois, se for troca de posição
 		 if (strcmp(nm[j - 1], nm[j]) > 0) {
 		    strcpy(aux, nm[j - 1]);
@@ -26,8 +26,8 @@ int main(void) {
 	}
 
 	// só mostrar a matriz
-	for (i = 0; i < 3; i++)
-	   printf("\n%s", nm[i]);
+	for (i = 0; i < 6; i++)
+	   printf("\n%s\n", nm[i]);
 
 	return 0;
 }
--- grupoAtiv/desafioParci4.c
+++ grupoAtiv/desafioParci4.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 
 int main(){
+	system("clear");
 
-int :
+	int d[10], e[10], i, j = 0;
+
+	for (i = 0; i <= 9; i++) {
+		printf("d[%d] <= :", i);
+		scanf("%d", &d[i]);
+		//e[i] = d[i];
+	}
+
+	
+	printf("\nVetor D:\n");
+	while(j <= 9){
+	printf("[%d]\t", d[i]);
+		j++;
+	}
+
+	printf("\n");
 
 	return 0;
 }
