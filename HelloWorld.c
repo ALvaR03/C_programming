8aacb02b97a9971a3007000e84a5d30240ebac1c
Alvaro Lima <alvarolima1103@outlook.com>
2020-04-15 18:26:52 -0300

    Alguns testes na liguagem

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//#include <string.h>
 
-int main() {
-	printf("Hello World!! \n");
+int main(void) {
+system("cls");
+	char texto[100];
+	int tam1, tam2;
+
+	printf("Digite o texto: ");
+	gets(texto);
+
+
+	printf("A quantidade de letra do texto e: %d", strlen(texto));
+
+/*
+	do
+	{
+		printf("Hello World!!");
+		printf("\nDeseja continuar? (S/N)");
+		scanf("%c", &resposta);
+		resposta = toupper(resposta);
+		fflush(stdin);
+	} while (resposta != 'N');
+*/
+
+
+
+	//printf("Hello World!! \n %%");
 	return 0;
 }
