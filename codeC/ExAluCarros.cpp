1ee88d443e35fc3866204c029fc36d7cd0a94ad9
Alvaro Lima <alvarolima1103@hotmail.com>
2020-08-29 19:40:22 -0400

    Exemplo da aula

---
+++ codeC/ExAluCarros.cpp
@@ -0,0 +1,249 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//declaracao de registros
+typedef struct Login {
+	char login[12];
+	int senha;
+}Login;
+
+typedef struct Contato{
+	char telefone[12];
+	char email[20];
+}Contato;
+
+typedef struct Endereco{
+	char rua[12];
+	char bairro[15];
+	char casa[12];
+}Endereco;
+
+typedef struct Funcionario{
+	char nome[15];
+	char cpf[16];
+	char rg[12];
+	Login login;
+	Contato cont;
+	Endereco end;
+}Funcionario;
+
+typedef struct Cliente{
+	char nome[15];
+	char cpf[16];
+	char rg[12];
+	int cnh;
+	Contato cont;
+	Endereco end;
+}Cliente;
+
+typedef struct Carro{
+	char marca[10];
+	char modelo[10];
+	char placa[10];
+	float vd;
+	char status[10];
+}Carro;
+
+int main() {
+    Login aux, login;
+    Funcionario vetfunc[3];
+    Carro vetcar[3];
+    Cliente vetcli[3];
+
+    int opL, opF;	
+    //inicializando ADM
+    strcpy(aux.login, "ADM\n");//comando para atribuicao de estring 
+    aux.senha = 123;
+	//___________________________________________________________
+	
+		do{
+			system("@cls || clear");
+			printf("Login: ");
+			setbuf(stdin, NULL);
+			fgets(login.login, sizeof(login.login), stdin);
+			printf("Senha: ");
+			scanf("%i", &login.senha);
+		
+			if((strcmp(login.login, aux.login) == 0) && (login.senha == aux.senha)){
+			 
+				 printf("Bem vindo Administrador\n");
+				 printf("Cadastre o funcionario para acesso\n");
+			 
+				 for(int i = 0; i < 3; i++){
+					 system("@cls || clear");
+					 
+					 printf("Dados Pessoais____________________________\n\n");
+					 printf("Nome: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].nome, sizeof(vetfunc[i].nome), stdin);
+					 
+					 printf("Cpf: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].cpf, sizeof(vetfunc[i].cpf), stdin);
+					 
+					 printf("RG: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].rg, sizeof(vetfunc[i].rg), stdin);
+					 
+					 printf("\nEndereco__________________________________\n\n");
+					 printf("Bairro: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].end.bairro, sizeof(vetfunc[i].end.bairro), stdin);
+					 
+					 printf("Rua: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].end.rua, sizeof(vetfunc[i].end.rua), stdin);
+					 
+					 printf("Casa: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].end.casa, sizeof(vetfunc[i].end.rua), stdin);
+					 
+					 printf("\nContato____________________________________\n\n");
+					 printf("Telefone: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].cont.telefone, sizeof(vetfunc[i].cont.telefone), stdin);
+					 
+					 printf("Email: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].cont.email, sizeof(vetfunc[i].cont.email), stdin);
+					 
+					 printf("\nDados de acesso____________________________________ \n\n");
+					 printf("Login: ");
+					 setbuf(stdin, NULL);
+					 fgets(vetfunc[i].login.login, sizeof(vetfunc[i].login.login), stdin);
+					 
+					 printf("Senha: ");
+					 scanf("%i",&vetfunc[i].login.senha); 
+				 }//fim for para preencher cadastro dos funcionarios
+			  
+			}//fim condicao de acesso(LOGIN) do administrador
+		
+		//logica para acesso do funcionario
+		for(int i = 0; i < 3; i++){
+			if((strcmp(login.login, vetfunc[i].login.login) == 0) && (login.senha == vetfunc[i].login.senha)){
+			do {
+				printf("\t\tBem vindo Funcionario:%s \n", vetfunc[i].nome);
+			  	printf("1-Cadastrar Carro\n");
+			  	printf("2-Cadastrar Cliente\n");
+			  	printf("3-Realizar aluguel Carro\n");
+			  	printf("4-Devolucao Carro\n");
+			  	printf("0-Sair\n");
+			  	scanf("%i", &opF);
+				switch(opF){
+                    case 1:
+                        for(int i = 0; i < 3; i++){
+                            printf("Marca: ");
+                            setbuf(stdin, NULL);	
+                            fgets(vetcar[i].marca, sizeof(vetcar[i].marca), stdin);
+
+                            printf("Modelo: ");
+                            setbuf(stdin, NULL);
+                            fgets(vetcar[i].modelo, sizeof(vetcar[i].modelo), stdin);
+
+                            printf("Placa: ");
+                            setbuf(stdin, NULL);
+                            fgets(vetcar[i].placa, sizeof(vetcar[i].placa), stdin);
+
+                            printf("Valor da diaria: ");
+                            scanf("%f", &vetcar[i].vd);
+                         
+                         strcpy(vetcar[i].status,"livre");
+                        }//fim for cadastro dos carros	
+                        break;//fim case 1	
+                    case 2:
+                        for(int i = 0; i < 3; i++){
+                            printf("Nome: ");
+                            setbuf(stdin, NULL);
+                            fgets(vetcli[i].nome, sizeof(vetcli[i].nome), stdin);
+
+                            printf("\nCpf: ");
+                            setbuf(stdin, NULL);
+                            fgets(vetcli[i].cpf, sizeof(vetcli[i].cpf), stdin);
+
+                            printf("\nRg: ");
+                            setbuf(stdin, NULL);
+                            fgets(vetcli[i].rg, sizeof(vetcli[i].rg), stdin);
+
+                            printf("\nCNH: ");
+                            scanf("%i",&vetcli[i].cnh);
+
+                            //cadastrar contato e endereco	
+
+                        }//fim for cadastro dos clientes	
+
+                        break;//fim case 2	
+                    case 3 :
+
+                        char cpfpesq[16];//variavel auxiliar para comparar cpf
+                        int achou,es,diaria;
+                        float vt;
+                        achou = 0;
+
+                        //achou=0;
+                        printf("Digite Cpf para consulta: ");
+                        setbuf(stdin, NULL);
+                        fgets(cpfpesq, sizeof(cpfpesq), stdin);
+
+                        for(int c = 0; c < 3; c++){
+                            if(strcmp(vetcli[c].cpf, cpfpesq) == 0){
+                                achou = 1;
+
+                                //exibir os carros
+                                for(int car = 0; car < 3; car++){
+                                    if(strcmp(vetcar[car].status, "livre") == 0){
+
+                                        printf("%i - Modelo:%s \t", car,vetcar[car].modelo);
+                                        printf("Marca:%s \t", vetcar[car].marca);
+                                        printf("Valor:%.2f \n", vetcar[car].vd);
+                                    }//fim if
+                                }//fim for lista de carros   
+
+                                printf("Escolha o carro desejado:\n");
+                                scanf("%d", &es);
+
+                                printf("Escolha o numero de diaria\n");
+                                scanf("%d", &diaria);
+
+
+                                vt = vetcar[es].vd * diaria;
+                                printf("Total do aluguel: %.2f \n", vt);
+                                strcpy(vetcar[es].status, "alugado");   
+
+                            }//fim if comparacao dos clientes
+                        }//fim for comparacao dos clientes
+
+                        if(achou == 0){
+                            printf("Cliente nao cadastrado\n");	
+                        }
+
+                        break;//fim case 3	
+				
+				case 4 :
+					break;	
+				case 0:
+					opF=0;
+					break;	
+				default:
+					printf("Opcao invalida\n");
+				}//fim switch
+				}while(opF!=0);//fim loop menu funcionario
+		  	
+		  }//fim if da condicao do funcionario
+		}//fim for para condicao de acesso funcionario
+		
+				
+		
+		
+		  printf("Desejar continuar no Sistema\n");
+		  printf("\n 1-Sim\n 0-Nao\n");
+		  scanf("%i",&opL);	
+		  if(opL==0){
+		  	exit(0);
+		  }
+		}while(opL != 0);
+	
+    return 0;	
+}//fim da main
+
+
--- codeC/motoSports.c
+++ codeC/motoSports.c
@@ -3,41 +3,63 @@
 #include <stdbool.h>
 #include <string.h>
 
-struct Clientes {
-    char nome[20];
-    int cpf; 
-    float numCnh;
-    int telefone;
-    char endereco[20];
-};
+typedef struct _Endereco {
+    char rua[50];
+    int casa;
+    char bairro[50];
+} Endereco;
 
-struct Funcionario {
+typedef struct _Contato {
+    char telefone[11];
+    char email[50];
+    Endereco ender;
+} Contato;
+
+typedef struct _Funcionario {
     char nome[20];
-    int cpf;
+    char cpf[15];
+    char rg[15];
     char login[15];
     char senha[15];
-};
+    Contato ctt;
+    Endereco ender;
+} Funcionario;
 
-struct Carro {
+typedef struct _Carro {
     char marca[10];
     char modelo[10];
     char placa[8];
-    int ano;
-    char cor[10];
     float valorDiaria;
-};
+} Carro;
+
+typedef struct _Clientes {
+    char nome[20];
+    char cpf[15];
+    char rg[15];
+    float numCnh;
+    Contato ctt;
+    Endereco ender;
+} Clientes;
+
+void clrscr() { // Essa funcao so linha a tela
+    system("@cls || clear");
+}
+
+void cadasFunc();
+
+
+
 
 int main(void) {
     //system("@cls || clear");
-    //Struct Arrays dos cadastros
-
-    struct Clientes cadasClientes[2];
-    int c;
-    struct Funcionario cadasFuncionario[2];
-    int f;
-    struct Carro cadasCarro[2];
-    int car;
     
+    //Struct Arrays dos cadastros
+    Clientes cadasClientes[2];
+    int c;
+    Funcionario arrayFuncionario[2];
+    int f;
+    Carro cadasCarro[2];
+    int car;
    //... 
 
     //Variavel do programa
@@ -60,218 +82,108 @@ int main(void) {
     int carregando, pesquisando;
 
         //Inicio
-        printf("~ Menu Principal ~\n");
-        printf("+================+\n");
-        printf("1 - Funcionario\n");
-        printf("2 - Cliente\n");
-        printf("0 - Sair\n");
-        printf("+================+\n");
-        printf("Opcao: ");
-        scanf("%d", &menuop);
+        printf("\t~ Tela inicial ~\n");
+        printf("+====================================+\n");
+        printf("Digite a senha para entrar no sistema!\n\n");
 
-        switch (menuop) {
-        case 1:
-            system("@cls || clear");
             
             do {
                 printf("Login: ");
-                fflush(stdin); //<--Limpar o buffer mas vc pode dar um espaco no %c pra funcionar
-                fgets(login, 50, stdin);
+                setbuf(stdin, NULL); //<--Limpar o buffer mas vc pode dar um espaco no %c pra funcionar
+                fgets(login, sizeof(login), stdin);
 
                 printf("Senha: ");
-                fflush(stdin);
-                fgets(senha, 50, stdin);
+                setbuf(stdin, NULL);
+                fgets(senha, sizeof(senha), stdin);
 
                 system("clear");
 
+                //printf("A senha e : %s\n", senha);
                 //Voce pode alterar o login e senha a qualquer momento por aqui ;D
-                if (!strcmp(login, "admin")  && !strcmp(senha, "2023")) {
+                if ((!strcmp(login, "admin") && !strcmp(senha, "2023")) == 0) {
                     printf("    ** Bem-Vindo **     \n");
                 } else {
-                    printf("Usuario nao encontrado tente novamente!\n");
+                    printf("Cadastro invelido tente novamente!\n");
                     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
                 }
-            } while ((!strcmp(login, "admin") && !strcmp(senha, "2023")) == false);
+            } while ((strcmp(login, "admin") && strcmp(senha, "2023")) == 0);
 
 
-            while (!strcmp(login, "admin") && !strcmp(senha, "2023")) {
-
-/*
-                int carga = 0; //Carragamento para efeitos visuais
-                while (carga <= 100) {
-                    system("clear");
-                    printf("Carregando!!\n %d %% \n", carga);
-                    carga++;
-                }
-                system("clear");// Fim do carragamento inutil
-*/
 
                 printf("|==============================|\n");
                 printf("|   ** Menu de Cadastro **     |\n");
                 printf("|==============================|\n");
-                printf("( 1 ) - Cadastrar Cliente       \n");
-                printf("( 2 ) - Cadastrar Funcionario   \n");
+                printf("( 1 ) - Cadastrar Funcionario   \n");
+                printf("( 2 ) - Cadastrar Cliente        \n");
                 printf("( 3 ) - Cadastrar Carro         \n");
                 printf("( 4 ) - Exibir Relatorio        \n");
                 printf("( 5 ) - Opcao para Cliente      \n");
                 printf("|==============================|\n");
                 printf("Opcao: ");
-                fflush(stdin);
+                //setbuf(stdin, NULL);
                 scanf("%d", &menufunc);
 
                 switch (menufunc) {
                 case 1:     //Cadastro de clientes
-                    system("cls");
-
-                       for(c = 0; c < 2; c++) {
-                       printf("-- Cadastro de Clientes --\n");
-                       printf("Nome: ");
-                       fflush(stdin);
-                       fgets(cadasClientes[c].nome, 20, stdin);
-                       printf("CPF: ");
-                       scanf("%d", &cadasClientes[c].cpf);
-                       printf("Numero da CNH: ");
-                       scanf("%d", &cadasClientes[c].numCnh);
-                       printf("Telefone: ");
-                       scanf("%d", &cadasClientes[c].telefone);
-                       printf("Endereco (Rua, Casa e Bairro): ");
-                       fflush(stdin);
-                       scanf("%d", &cadasClientes[c].endereco);
-                       system("cls");
-
-
-                       printf("## O cliente foi cadastrados! ##\n");
-                       printf("Deseja continuar cadastrando? \n");
-                       printf("1 - Sim \n");
-                       printf("0 - Nao \n");
-                       printf("Opcao: ");
-                       fflush(stdin);
-                       scanf("%d", &opsn);
-                       system("cls");
-
-                           if (opsn == 1) { 
-                               continue;
-                            }
-                       } 
-                    
-                    break;
-                
+                    //clrscr();
+                    cadasFunc();
 
                 case 2: //Cadastro de Funcionarios
-                    system("cls");
-
-                    for(f = 0; f < 2; f++) {
-                        printf("-- Cadastro de Funcionarios --\n");
-                        printf("Nome: ");
-                        fflush(stdin);
-                        fgets(cadasFuncionario[f].nome, 20, stdin);
-                        printf("Cpf: ");
-                        scanf("%d", &cadasFuncionario[f].cpf);
-                        printf("Login: ");
-                        fflush(stdin);
-                        fgets(cadasFuncionario[f].login, 15, stdin);
-                        printf("Senha: ");
-                        fflush(stdin);
-                        fgets(cadasFuncionario[f].senha, 15, stdin);
-                        system("cls");
-
-                        printf("## O funcionario foi cadastrados! ##\n");
-                        printf("Deseja continuar cadastrando? \n");
-                        printf("1 - Sim \n");
-                        printf("0 - Nao \n");
-                        printf("Opcao: ");
-                        fflush(stdin);
-                        scanf("%d", &opsn);
-                        system("cls");
-
-                            if (opsn != 1) { 
-                                break;
-                            }
-                    }
+                    clrscr();
 
                 break;
-
 
                 case 3: //Cadastro de carros
-                system("cls");
-                do {
-                    for(car = 0; car < 2; car++) {
-                        printf("-- Cadastro de Carros --\n");
-                        printf("Marca: ");
-                        fflush(stdin);
-                        fgets(cadasCarro[car].marca, 10, stdin);
-                        printf("Modelo: ");
-                        fflush(stdin);
-                        fgets(cadasCarro[car].modelo, 10, stdin);
-                        printf("Placa: ");
-                        fflush(stdin);
-                        fgets(cadasCarro[car].placa, 8, stdin);
-                        printf("Ano: ");
-                        fflush(stdin);
-                        scanf("%d", &cadasCarro[car].ano);
-                        printf("Cor: ");
-                        fflush(stdin);
-                        fgets(cadasCarro[car].cor, 10, stdin);
-                        printf("Valor da diaria: ");
-                        fflush(stdin);
-                        scanf("%f", &cadasCarro[car].valorDiaria);
-                        system("cls");
-
-                    }
-                    car++;
-                } while (car == 2);
 
                 break;
-
-                case 4: //Consultar carro cadastrado
-                system("cls");
-                acesso = true;
-
-                    printf("Procurar por placa: ");
-                    fflush(stdin);
-                    gets(placaPesq);
-
-                    for(car = 0; car < 2; car++) {
-                        if(placaPesq == cadasCarro[car].placa) {
-                            printf("Marca: %s", cadasCarro[car].marca);
-                            printf("Modelo: %s", cadasCarro[car].modelo);
-                            printf("Placa: %s", cadasCarro[car].placa);
-                            printf("Ano: %d", cadasCarro[car].ano);
-                            printf("Cor: %s", cadasCarro[car].cor);
-                            printf("Valor da diaria: %.2f", cadasCarro[car].valorDiaria);
-                            system("cls");
-                        }
-                    }
-
-                    if(acesso == false) {
-                        printf("");
-                        printf("\nPlaca nao encontrada!!");
-                    }
-
-                    system("pause");
-
-                break;
-
                 case 5:
                     if (menufunc == 5) {
                         exit;
                     }
                 break;
                 
-                    default:
-                        printf("Opcao desconhecida!!!!");
-                        break;
+                default:
+                    printf("Opcao desconhecida!!!!");
+                    break;
                 }
-            }
-            
-            
-
-            break;
-        
-        default:
-            printf("\nObrigado e volte sempre!!\n");
-            break;
-        }
-
     return 0;
+}
+
+void cadasFunc() {
+   printf("-- Cadastro de Funcionario --\n");
+    /*
+       for(c = 0; c < 2; c++) {
+       printf("-- Cadastro de Clientes --\n");
+       printf("Nome: ");
+       fflush(stdin);
+       fgets(cadasClientes[c].nome, 20, stdin);
+       printf("CPF: ");
+       scanf("%d", &cadasClientes[c].cpf);
+       printf("Numero da CNH: ");
+       scanf("%d", &cadasClientes[c].numCnh);
+       printf("Telefone: ");
+       scanf("%d", &cadasClientes[c].telefone);
+       printf("Endereco (Rua, Casa e Bairro): ");
+       fflush(stdin);
+       scanf("%d", &cadasClientes[c].endereco);
+       system("cls");
+
+
+       printf("## O cliente foi cadastrados! ##\n");
+       printf("Deseja continuar cadastrando? \n");
+       printf("1 - Sim \n");
+       printf("0 - Nao \n");
+       printf("Opcao: ");
+       fflush(stdin);
+       scanf("%d", &opsn);
+       system("cls");
+
+           if (opsn == 1) { 
+               continue;
+            }
+       } 
+    
+    break;
+*/ 
+
 }
