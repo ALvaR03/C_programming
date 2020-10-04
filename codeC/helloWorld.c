9fb64bf7390d3e8ca68b18a74c3a3e20db3ad149
Alvaro Lima <alvarolima1103@hotmail.com>
2020-10-04 08:22:39 -0400

    problema resolvido

--- codeC/helloWorld.c
+++ codeC/helloWorld.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main(void) {
 
-    for (int i = 0; i <= 5; ++i) {
-        printf("Repetido: %d\n", i);
-    }
+    char a[15], b[15], c[15];
+    fgets(a, sizeof(a), stdin);
+    fgets(b, sizeof(b), stdin);
+    fgets(c, sizeof(c), stdin);
 
+    if (a[0] == 'v') {
+        if (b[0] == 'a') {
+            if (c[0] == 'c') {
+                printf("aguia\n");
+            } else {
+                printf("pomba\n");
+            }
+        } else {
+            if(c[0] == 'o') printf("homen\n");
+        }
+    }
+    
     return 0;
 }
